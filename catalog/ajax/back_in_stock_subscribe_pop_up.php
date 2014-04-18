c45762a189ebddc1866c052b67d12057188fbf8b
bislewl <bislewl@gmail.com>
2014-04-17 19:03:57 -0500

    fixes

--- catalog/ajax/back_in_stock_subscribe_pop_up.php
+++ catalog/ajax/back_in_stock_subscribe_pop_up.php
@@ -17,41 +17,13 @@ $name = zen_db_prepare_input($_POST['contactname']);
 $email_address = zen_db_prepare_input($_POST['email']);
 $enquiry = zen_db_prepare_input(strip_tags($_POST['enquiry']));
 $antiSpam = isset($_POST['should_be_empty']) ? zen_db_prepare_input($_POST['should_be_empty']) : '';
-$zco_notifier->notify('NOTIFY_CONTACT_US_CAPTCHA_CHECK');
+
 $product_id = zen_db_prepare_input($_POST['product_id']);
 
 $zc_validate_email = zen_validate_email($email_address);
 
 if($zc_validate_email && $antiSpam == '' && $product_id != ''){
- /*
-  // Prepare extra-info details
-  $extra_info = email_collect_extra_info($name, $email_address, $customer_name, $customer_email);
-  // Prepare Text-only portion of message
-  $text_message = OFFICE_FROM . "\t" . $name . "\n" .
-  OFFICE_EMAIL . "\t" . $email_address . "\n\n" .
-  '------------------------------------------------------' . "\n\n" .
-  strip_tags($_POST['enquiry']) .  "\n\n" .
-  '------------------------------------------------------' . "\n\n" .
-  $extra_info['TEXT'];
-  // Prepare HTML-portion of message
-  $html_msg['EMAIL_MESSAGE_HTML'] = strip_tags($_POST['enquiry']);
-  $html_msg['CONTACT_US_OFFICE_FROM'] = OFFICE_FROM . ' ' . $name . '<br />' . OFFICE_EMAIL . '(' . $email_address . ')';
-  $html_msg['EXTRA_INFO'] = $extra_info['HTML'];
-  // Send message
-  if($setemailarr == 1 && is_array($emailarraysend)){
-  
-for ($i = 0; $i < count($emailarraysend); $i++) {
-$emailtosendarr='';
-$emailtosendarr = explode("||", $emailarraysend[$i]);
-  zen_mail($emailtosendarr[0], $emailtosendarr[1], EMAIL_SUBJECT, $text_message, $name, $email_address, $html_msg,'contact_us');
- 
-  }
-  
-  }else{
-  zen_mail($send_to_name, $send_to_email, EMAIL_SUBJECT, $text_message, $name, $email_address, $html_msg,'contact_us');
- 
-  }
- */
+
   
   $bis['email'] = $email_address;
   $bis['name'] = $name;
