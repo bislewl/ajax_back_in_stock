54b8612d1f1ecdaea5c4b01a4a665df38a6b7dc4
Larry Bislew <bislewl@gmail.com>
2015-02-12 16:53:31 -0600

    modified js for product listing page

--- catalog/includes/templates/YOUR_TEMPLATE/jscript/jquery/jquery_back_in_stock.js
+++ catalog/includes/templates/YOUR_TEMPLATE/jscript/jquery/jquery_back_in_stock.js
@@ -14,4 +14,28 @@ $(document).ready(function () {
             });
         }
     });
+    $('a.back-in-stock-listing-popup-link').click(function(event) {
+        event.preventDefault();
+    var productDiv = $(this).parent();
+    $('#back-in-stock-product-image img').attr('src',$(productDiv).find('.lazy').attr('src'));
+    $('#productName').html($(productDiv).find('span.itemTitle').text());
+    $('input[name="product_id"]').attr('value',$(productDiv).find('input[name="bis-product-id"]').attr('value'));
+    $.fancybox({
+        href: '#back-in-stock-popup-wrapper', 
+        modal: true,
+        afterShow: function () {
+            $(document).on('submit', '#back-in-stock-popup-wrapper form[name="back_in_stock"]', function () {
+                $('#contact_messages').empty();
+                $.post('ajax/back_in_stock_subscribe_pop_up.php', $('#back-in-stock-popup-wrapper form[name="back_in_stock"]').serialize(), function (data) {
+                    $('#contact_messages').html(data);
+                    if ($('.messageStackSuccess').length) {
+                        $('.back-in-stock-popup-wrapper-button-row').hide();
+                        $('.back-in-stock-popup-content-wrapper').hide();
+                    }
+                });
+                return false;
+            });
+        }
+    });
+  });
 });
