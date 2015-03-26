ab954857b39424e64680b23fbf727b09a505095f
Larry Bislew <bislewl@gmail.com>
2015-03-26 11:36:31 -0500

    added Italian Lang Files

---
+++ catalog/includes/languages/italian/extra_definitions/back_in_stock.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright Copyright 2010-2014  ZenCart.codes Owned & Operated by PRO-Webs, Inc. 
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+define('BACK_IN_STOCK_EMAIL', 'Email: ');
+define('BACK_IN_STOCK_NAME', 'Nome: ');
+define('BACK_IN_STOCK_TITLE', 'Notifiche disponibilita\' articoli');
+define('BACK_IN_STOCK_DESC', 'Per cancellare le notifiche selezionare il prodotto e cliccare \'Rimuovi\'');
+define('BACK_IN_STOCK_BUTTON', 'Rimuovi');
+define('BACK_IN_STOCK_CURRENT', 'Notifiche sottoscritte per: ');
+define('BACK_IN_STOCK_FIELD1', 'Rimuovi');
+define('BACK_IN_STOCK_FIELD2', 'Prodotto');
+define('BACK_IN_STOCK_FIELD3', 'Data sottoscrizione');
+define('BACK_IN_STOCK_NONE', 'Nessuna sottoscrizione');
+define('BACK_IN_STOCK_LINK', 'Avvisami quando sara\' disponibile');
+define('BACK_IN_STOCK_POPUP_HEADING', 'Purtroppo il prodotto e\' esaurito!');
+define('BACK_IN_STOCK_POPUP_SUBHEADING', 'Riempi i campi sottostanti e verrai avvisato quando il prodotto sara\' di nuovo disponibile <br/><font size="1">Clicca una sola volta e attendi alcuni secondi</font>');
+define('BACK_IN_STOCK_SEND_TEXT', 'Avvisami!');
+define('BACK_IN_STOCK_SUCCESS', 'Hai sottoscritto le notifiche');
+define('BACK_IN_STOCK_NAME_ERROR', 'Per favore controlla il nome');
+define('BACK_IN_STOCK_EMAIL_ADDRESS_ERROR', 'Per favore controlla l\'indirizzo Email');
+define('BACK_IN_STOCK_NO_PRODUCT_ERROR', 'Per favore seleziona un prodotto');
+define('BACK_IN_STOCK_ALREADY_SUB', 'Hai già sottoscritto le notifiche');
+define('BACK_IN_STOCK_MY_ACCOUNT_LINK', 'Rimuovi le notifiche della disponibilita\' prodotti');
