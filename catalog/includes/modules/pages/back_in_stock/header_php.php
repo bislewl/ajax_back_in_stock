fc229c42f6a8218961dc28609072eeb18ec9c1f5
Larry Bislew <bislewl@gmail.com>
2015-09-16 10:27:34 -0500

    added support for those browsers that don't honor the ajax call: Firefox in Windows 10

--- catalog/includes/modules/pages/back_in_stock/header_php.php
+++ catalog/includes/modules/pages/back_in_stock/header_php.php
@@ -24,7 +24,15 @@ if (is_array($_POST['bis_id'])) {
             back_in_stock_subscription(array('bis_id' => $bis_id), "delete");
             break;
     }
-    $bis_id_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE bis_id='" . zen_db_prepare_input($_POST['bis_id'][0])."' AND sub_active=1");
+    $bis_id_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE bis_id='" . zen_db_prepare_input($_POST['bis_id'][0]) . "' AND sub_active=1");
+    if ($bis_id_info->RecordCount() > 0) {
+        $subcriptions = true;
+        $email_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE email LIKE '" . $bis_id_info->fields['email'] . "' AND sub_active=1");
+    } else {
+        $subcriptions = false;
+    }
+} elseif ((int) $_GET['bis_id'] > 0) {
+    $bis_id_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE bis_id='" . (int) $_GET['bis_id'] . "' AND sub_active=1");
     if ($bis_id_info->RecordCount() > 0) {
         $subcriptions = true;
         $email_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE email LIKE '" . $bis_id_info->fields['email'] . "' AND sub_active=1");
@@ -32,15 +40,44 @@ if (is_array($_POST['bis_id'])) {
         $subcriptions = false;
     }
 }
-else{
-    if((int)$_GET['bis_id'] > 0){
-        $bis_id_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE bis_id='" . (int)$_GET['bis_id']."' AND sub_active=1");
-    if ($bis_id_info->RecordCount() > 0) {
-        $subcriptions = true;
-        $email_info = $db->Execute("SELECT * FROM " . TABLE_BACK_IN_STOCK . " WHERE email LIKE '" . $bis_id_info->fields['email'] . "' AND sub_active=1");
-    } else {
-        $subcriptions = false;
+$get_action = zen_db_prepare_input($_GET['action']);
+if ($get_action == 'send') {
+    $error = false;
+    $name = zen_db_prepare_input($_POST['customer_name']);
+    $email_address = zen_db_prepare_input($_POST['email']);
+    $empty = isset($_POST['should_be_empty']) ? zen_db_prepare_input($_POST['should_be_empty']) : '';
+    $product_id = zen_db_prepare_input($_POST['product_id']);
+    //Validate the email address
+    $zc_validate_email = zen_validate_email($email_address);
+    if (sizeof($_SESSION['navigation']->snapshot) > 0) {
+        $old_page = $_SESSION['navigation']->snapshot['page'];
     }
+    if ($zc_validate_email && $empty == '' && $product_id != '') {
+        $bis['email'] = $email_address;
+        $bis['name'] = $name;
+        $bis['product_id'] = $product_id;
+        $returned_results = back_in_stock_subscription($bis, "add");
+        if ($returned_results === "Subscribed") {
+            $messageStack->add_session($old_page, BACK_IN_STOCK_SUCCESS, 'success');
+        } else {
+            $messageStack->add_session($old_page, $returned_results, 'caution');
+        }
+    } else {
+        $error = true;
+        if (empty($name)) {
+            $messageStack->add_session($old_page, BACK_IN_STOCK_EMAIL_ADDRESS_ERROR, 'caution');
+        }
+        if ($zc_validate_email == false) {
+            $messageStack->add_session($old_page, BACK_IN_STOCK_EMAIL_ADDRESS_ERROR, 'caution');
+        }
+        if ($product_id == '') {
+            $messageStack->add_session($old_page, BACK_IN_STOCK_NO_PRODUCT_ERROR, 'caution');
+        }
+        $messageStack->add_session($old_page, $returned_results, 'caution');
+    }
+    if (sizeof($_SESSION['navigation']->snapshot) > 0) {
+        $origin_href = zen_href_link($_SESSION['navigation']->snapshot['page'], zen_array_to_string($_SESSION['navigation']->snapshot['get'], array(zen_session_name())), $_SESSION['navigation']->snapshot['mode']);
+        zen_redirect($origin_href);
     }
 }
 if ($_SESSION['customer_id']) {
